5bdc117843dcb9ef6defb543d758b7785c6b544e
Daniel Arrais <danielarraiscarvalho@gmail.com>
2025-01-12 19:20:47 -0300

    Create spring base app

---
+++ src/main/java/dev/danielarrais/springandkafka/SpringAndKafkaApplication.java
@@ -0,0 +1,13 @@
+package dev.danielarrais.springandkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndKafkaApplication.class, args);
+    }
+
+}
